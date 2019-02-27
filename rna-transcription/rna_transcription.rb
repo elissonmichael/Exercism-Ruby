5aaeb8ca8311aaa8c3d9d76bc79bf1081b96c5c4
Élisson Michael <elissonmichael@yahoo.com.br>
2019-02-27 14:11:40 -0300

    Side Exercise 1 | Refactor to Reduce Method Complexity

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,6 +1,5 @@
 class Complement
-  CONVERSION = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }
   def self.of_dna(sequence)
-    sequence.chars.map { |char| CONVERSION[char] }.join
+    sequence.tr('CGTA', 'GCAU')
   end
 end
