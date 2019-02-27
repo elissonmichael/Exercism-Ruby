b10ecc0f928355608652d52f961e3224379c0e0f
Élisson Michael <elissonmichael@yahoo.com.br>
2019-02-27 13:54:05 -0300

    Exercise 2 | Refactor for Conditional Removal

--- two-fer/two_fer.rb
+++ two-fer/two_fer.rb
@@ -1,9 +1,5 @@
 class TwoFer
-  def self.two_fer(name = nil)
-    if name
-      "One for #{name}, one for me."
-    else
-      'One for you, one for me.'
-    end
+  def self.two_fer(name = 'you')
+    "One for #{name}, one for me."
   end
 end
