00c4e497ac93df55969775d988a49773a6cb82b6
Élisson Michael <elissonmichael@yahoo.com.br>
2019-03-04 18:49:24 -0300

    Exercise 8 | Refactor for Better Readability

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,8 +1,12 @@
+class StrandLengthError < ArgumentError
+
+end
+
 class Hamming
-  def self.compute(dna1,dna2)
-    raise ArgumentError unless dna1.size.eql? dna2.size
-    (0..dna1.size).map    { |index| dna1[index].eql? dna2[index] }
-                  .select { |bool|  bool.eql? false }
-                  .size
+  def self.compute(strand1, strand2)
+    unless strand1.size.eql? strand2.size
+      raise StrandLengthError, 'Strands must be same length'
+    end
+    (0..strand1.size).count { |index| strand1[index] != strand2[index] }
   end
 end
