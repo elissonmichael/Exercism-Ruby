2f4fbc582cb1c9e54796413822d71ed4567e523b
Élisson Michael <elissonmichael@yahoo.com.br>
2019-03-09 13:11:05 -0300

    Exercise 10 | Refactor to Reduce Code Complexity

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,13 +1,7 @@
 class Isogram
   def self.isogram?(string)
     @input = string
-    char_count.values.reject{ |count| count.equal? 1 }.empty?
-  end
-
-  private
-
-  def self.char_count
-    chars.each_with_object(Hash.new(0)) { |char, hash| hash[char] += 1 }
+    chars.eql?(chars.uniq)
   end
 
   def self.chars
