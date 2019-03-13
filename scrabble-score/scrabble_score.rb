489c96bcdfc8ad09caf91adcfd4d0424d1d6b6c9
Élisson Michael <elissonmichael@yahoo.com.br>
2019-03-13 02:17:04 -0300

    Exercise 11 | Refactor to Remove Unnecessary Class

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,37 +1,29 @@
-class ScrabbleChar
-  SCORE = {
-    %w[A E I O U L N R S T] => 1,   %w[D G] => 2, %w[B C M P] => 3,
-    %w[F H V W Y] => 4, %w[K] => 5, %w[J X] => 8, %w[Q Z] => 10
-  }.freeze
-
-  def initialize(char)
-    @char = char.upcase
-  end
-
-  def points
-    SCORE.each { |array, value| return value if array.include?(@char) }
-    0
-  end
-end
-
 class Scrabble
   def self.score(input)
     new(input).score
   end
 
+  SCORE = {
+    %w[A E I O U L N R S T] => 1,   %w[D G] => 2, %w[B C M P] => 3,
+    %w[F H V W Y] => 4, %w[K] => 5, %w[J X] => 8, %w[Q Z] => 10
+  }.freeze
+
   def initialize(input)
-    @input = input
+    @input = input || ''
   end
 
   def score
-    return 0 unless @input
-
-    chars.sum(&:points)
+    chars.sum { |char| points(char)}
   end
 
   private
 
+  def points(letter)
+    SCORE.each { |array, value| return value if array.include?(letter) }
+    0
+  end
+
   def chars
-    @input.scan(/\w/).map { |char| ScrabbleChar.new(char) }
+    @input.upcase.scan(/\w/)
   end
 end
