02f987df00e67fd8931f3bee8b3e88bceb908483
Élisson Michael <elissonmichael@yahoo.com.br>
2019-04-07 13:32:41 -0300

    Side Exercise 3 | Refactor to Use Simpler Structure

--- resistor-colors/resistor_colors.rb
+++ resistor-colors/resistor_colors.rb
@@ -1,8 +1,7 @@
 class ResistorColors
-  COLORMAP = { black: '0', brown: '1', red: '2', orange: '3', yellow: '4',
-               green: '4', blue: 6, violet: '7', grey: '8', white: '9' }.freeze
+  COLORMAP = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(colors)
-    colors.map { |color| COLORMAP[color.to_sym] }.join.to_i
+    colors.map { |color| COLORMAP.index(color) }.join.to_i
   end
 end
