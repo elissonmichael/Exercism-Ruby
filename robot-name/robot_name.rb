b27ffec8105bf0364ce8d051e5c1efd784ca0f32
Élisson Michael <elissonmichael@yahoo.com.br>
2019-08-05 09:09:59 -0300

    Side Exercise 8 | Refactor to Extract Class

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,29 +1,9 @@
-require 'set'
-class Robot
-  attr_reader :name
-  @@used_names = Set.new
-
-  def self.forget
-    @@used_names.clear
-  end
- 
-  def initialize
-    reset
-  end
-
-  def reset
-    unique_name = generate_random_name
-    unique_name = generate_random_name until @@used_names.add? unique_name
-    @name       = unique_name
-  end
-
-  private
-
-  def generate_random_name
-    random_name = ''
-    2.times { random_name << random_letter }
-    3.times { random_name << random_digit  }
-    random_name    
+class RobotNamer
+  def random
+    String.new.tap do |random_name|
+      2.times { random_name << random_letter }
+      3.times { random_name << random_digit  }
+    end    
   end
 
   def random_letter
@@ -32,5 +12,26 @@ class Robot
 
   def random_digit
     rand(0..9).to_s
+  end
+end
+
+require 'set'
+class Robot
+  attr_reader :name, :robot_namer
+  @@used_names = Set.new
+
+  def self.forget
+    @@used_names.clear
+  end
+
+  def initialize(robot_namer: RobotNamer.new)
+    @robot_namer = robot_namer
+    reset
+  end
+
+  def reset
+    unique_name = robot_namer.random
+    unique_name = robot_namer.random until @@used_names.add? unique_name
+    @name       = unique_name
   end
 end
